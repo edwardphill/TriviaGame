575b2c730c5f008e5b3ff3a729dab70e9094576b
Edward Hill <edwardpassanohill@gmail.com>
2019-11-19 21:51:54 -0500

    add psudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,21 @@
-// alert("this page is linked");
+// once page loads, start timer
+
+// if timer expires before all asnwers complete
+
+// store correct answers
+
+// if you click q1 A, B, C, or D, log value
+// if you click q2 A, B, C, or D, log value
+// if you click q3 A, B, C, or D, log value
+// if you click q4 A, B, C, or D, log value
+// if you click q5 A, B, C, or D, log value
+// if you click q6 A, B, C, or D, log value
+
+// if you submit answers for all 6 questions grade the submission
+
+// if the LETTER element you click on in q1 is correct, ++ score
+// if the LETTER element you click on in q2 is correct, ++ score
+// if the LETTER element you click on in q3 is correct, ++ score
+// if the LETTER element you click on in q4 is correct, ++ score
+// if the LETTER element you click on in q5 is correct, ++ score
+// if the LETTER element you click on in q6 is correct, ++ score
