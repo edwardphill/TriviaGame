b25d4145281fe43f6872de1b24a5ec92207bf859
Edward Hill <edwardpassanohill@gmail.com>
2019-11-22 12:46:43 -0500

    Add Question objects and choice arrays, add new html page to add quesiton elements dymanically, start logic for writing questions, choices on doc

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,90 @@
 // Psudocode from MRusso
 
+/*
+var currentQuestion = 0,
+  var timeLeft = 20;
+  var questionTimer = setInterval((timeLeft--) => {
+      if (timeLeft <= 0) {
+currentQuestion++;
+          renderTimesUpMessage ();
+          setTimeout(() => {
+              renderQuestion
+          }, timeout);
+      }
+      
+  }, 1000);
+
+  */
+
 var questions = [
   {
-    prompt:
+    questionText:
       "Who is attributed as writing the first peice of fishing literature ever and at what date?",
-    A: "A. Dame Juliana Berners, 'A Treatyse of Fysshynge wyth an Angle', 1486",
-    B: "Brad Pitt, 'A River Runs Through It', 1992",
-    C: "Ernest Hemingway, 'Old Man And The Sea', 1952",
-    D: "Izaak Walton, 'Compleat Angler', 1653",
-    answer: "a"
+    choices: [
+      "A. Dame Juliana Berners, 'A Treatyse of Fysshynge wyth an Angle', 1486",
+      "Brad Pitt, 'A River Runs Through It', 1992",
+      "Ernest Hemingway, 'Old Man And The Sea', 1952",
+      "Izaak Walton, 'Compleat Angler', 1653"
+    ],
+    answer: "A"
   },
+
   {
-    prompt:
+    questionText:
       "Who is wideley credited as being the first angler to us  artificial flies (ie, not worms live bait)?",
-    A: "Jeremy Wade in 2011 on Discovery Channel's TV show 'River Monsters'",
-    B:
-      "Roman author Claudius Aelianus near the end of the 2nd century.C. Teddy Roosevelt, 1914 in the Amzon Rainforest.",
-    C: "Teddy Roosevelt, 1914 in the Amzon Rainforest",
-    D: "No one does this.",
+    choices: [
+      "A. Jeremy Wade in 2011 on Discovery Channel's TV show 'River Monsters'",
+      "B. Roman author Claudius Aelianus near the end of the 2nd century.C. Teddy Roosevelt, 1914 in the Amzon Rainforest.",
+      "C. Teddy Roosevelt, 1914 in the Amzon Rainforest",
+      "D. No one does this."
+    ],
     answer: "B"
   },
   {
-    prompt: "Catch and release refers to?",
-    A: "A mid-2000's CLASSIC film with Jennifer Garner.",
-    B: "Dating in NYC.",
-    C:
-      "A fishing style where you land the fish and return it back to, the river or ocean to live another day.",
-    D: "When an umpire ejects a catcher from a baseball game.",
+    questionText: "Catch and release refers to?",
+    choices: [
+      "A. A mid-2000's CLASSIC film with Jennifer Garner.",
+      "B. Dating in NYC.",
+      "C. A fishing style where you land the fish and return it back to, the river or ocean to live another day.",
+      "D. When an umpire ejects a catcher from a baseball game."
+    ],
     answer: "C"
   },
   {
-    prompt: "Which of these is NOT a kind or type of fishing?",
-    A: "Trolling",
-    B: "Fly fishing",
-    C: "Drone fishing",
-    D: "Hydro-foiling",
+    questionText: "Which of these is NOT a kind or type of fishing?",
+    choices: [
+      "A. Trolling",
+      "B. Fly fishing",
+      "C. Drone fishing",
+      "D. Hydro-foiling"
+    ],
     answer: "D"
   },
   {
-    prompt: "Where is the home to the world most famous fish?",
-    A: "P. Sherman 42 Wallaby Way, Sidney, Australia",
-    B: "Loch Ness, Scotland",
-    C: "Dr. Evil's pool for sharks with *lasers*",
-    D: "Jiro's suhsi shop, Japan",
+    questionText:
+      "Where is the home to the one of the world most famous fish, who also shares the name for the Captain of the submarine Nautilus in novel '20,000 Leagues Under The Sea?'",
+    choices: [
+      "A. P. Sherman 42 Wallaby Way, Sidney, Australia",
+      "B. Loch Ness, Scotland",
+      "C. Dr. Evil's pool for sharks with *lasers*",
+      "D. Jiro's suhsi shop, Japan"
+    ],
     answer: "A"
   }
 ];
+
+$("#question").html(questions[1].questionText);
+
+// display only q1,
+// index or answer
+// grab question from question array, grab index of quetions array,
+// render quetion 0, pass it some quesiton objuect to pass it onto the screen, take object buld emelemt for quesitons,
+// buid element for choice, add click listener for all chocies, as soon as click choice, cancel timer,
+// firs check to see if = to answer, then empty container, the render results, "you got the anser correct" set timeout for 3 second, empy success
+// current quesiton 1, render
+
+/*
+  
 
 var timeResest;
 var wins = 0;
@@ -62,7 +101,7 @@ for (var i = 0; i < questions.length; i++) {
 
 // once page loads, start timer
 
-// display only q1,
+
 
 
 
